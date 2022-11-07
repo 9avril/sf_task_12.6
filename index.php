2faf172f84fede2df73f5528f670d0cef1deddbc
fluevel <artemkiasko@gmail.com>
2022-11-07 11:50:17 +0300

    init

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+$example_persons_array = [
+    [
+        'fullname' => 'Иванов Иван Иванович',
+        'job' => 'tester',
+    ],
+    [
+        'fullname' => 'Степанова Наталья Степановна',
+        'job' => 'frontend-developer',
+    ],
+    [
+        'fullname' => 'Пащенко Владимир Александрович',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Громов Александр Иванович',
+        'job' => 'fullstack-developer',
+    ],
+    [
+        'fullname' => 'Славин Семён Сергеевич',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Цой Владимир Антонович',
+        'job' => 'frontend-developer',
+    ],
+    [
+        'fullname' => 'Быстрая Юлия Сергеевна',
+        'job' => 'PR-manager',
+    ],
+    [
+        'fullname' => 'Шматко Антонина Сергеевна',
+        'job' => 'HR-manager',
+    ],
+    [
+        'fullname' => 'аль-Хорезми Мухаммад ибн-Муса',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Бардо Жаклин Фёдоровна',
+        'job' => 'android-developer',
+    ],
+    [
+        'fullname' => 'Шварцнегер Арнольд Густавович',
+        'job' => 'babysitter',
+    ],
+];
+
+
+function getFullnameFromParts($surname, $name, $patronymic)
+{
+    return $surname . ' ' . $name . ' ' . $patronymic;
+}
+
+
+function getPartsFromFullname($fio)
+{
+    $id_human = ['surname', 'name', 'patronomic'];
+    $expfio = explode(' ', $fio);
+    return array_combine($id_human, $expfio);
+}
+
+function getShortName($fio)
+{
+    $expfio = explode(' ', $fio);
+    return $expfio[1] . ' ' . substr($expfio[0], 0, 2) . '.';
+}
+
+
